fb424b56739f9fc5b0b764d1c718ccb5e59266e9
Hager-Elkhouly <162358692+Hager-Elkhouly@users.noreply.github.com>
2024-12-14 12:49:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example for form validation or other JS functionality
+
+document.querySelector("form").addEventListener("submit", function(event) {
+                                                                                                    event.preventDefault();
+                                                                                                    alert("تم إرسال رسالتك بنجاح!");
+                                                                                                });
